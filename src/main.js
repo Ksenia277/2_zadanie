ddeecd48fbf280643922eb99945528f1d31fd1d9
Ksenia277 <shahhovakseenis.2005@mail.ru>
2024-02-17 01:43:01 +0700

    добавление методов для перехода процентов и ограничений

--- src/main.js
+++ src/main.js
@@ -48,13 +48,31 @@ let app = new Vue({
             const checkedCount = card.items.filter(item => item.checked).length;
             const progress = (checkedCount / card.items.length) * 100;
             card.isComplete = progress === 100;
+
+            if (progress >= 50 && !card.isComplete) {
+                this.moveCardBetweenColumns(card, this.firstColumn, this.secondColumn);
+            } else if (card.isComplete && this.secondColumn.includes(card)) {
+                this.moveCardBetweenColumns(card, this.secondColumn, this.thirdColumn);
+            }
             if (progress === 0 && this.secondColumn.length < 5) {
                 this.moveCardBetweenColumns(card, this.firstColumn, this.secondColumn);
             }
         },
         moveCardBetweenColumns(card, fromColumn, toColumn) {
             const index = fromColumn.findIndex(c => c.id === card.id);
+            if (index !== -1) {
+                if (toColumn === this.secondColumn && this.secondColumn.length >= 5) {
+                    alert('Во втором столбце не может быть больше 5 карточек.');
+                    return;
+                }
 
+                fromColumn.splice(index, 1);
+                toColumn.push(card);
+
+                if (fromColumn === this.firstColumn && toColumn === this.secondColumn) {
+                    this.disableCheckboxes(card);
+                }
+            }
         },
 
     },
